264e0bfae6302bc2d49e7e857eee78218f63557c
vishal.Alok <you@example.com>
2021-10-18 06:50:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Animal
+{
+    public virtual void eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+public class Dog : Animal
+{
+    public override void eat()
+    {
+        Console.WriteLine("Eating bread...");
+    }
+}
+public class TestOverriding
+{
+    public static void Main()
+    {
+        Dog d1 = new Dog();
+        Animal d2 = d1;
+        d1.eat();
+        d2.eat();
+    }
+}
